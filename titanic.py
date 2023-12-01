7649ad92c83d43c1f0df546f0273e0ef579c51cb
gmusmus <tokarevav@gmail.com>
2023-12-01 21:23:25 +0700

    Описание ваших изменений

--- titanic.py
+++ titanic.py
@@ -11,17 +11,20 @@ except ModuleNotFoundError:
         print(f"Failed to install required modules: {e}")
         exit(1)
 
-    # Попробуйте импортировать снова после установки
-    import pandas as pd
-    from catboost.datasets import titanic
-
-# Остальной код вашего скрипта
-# ...
-
 # Загрузка данных о пассажирах "Титаника"
 train_df, _ = titanic()
 titanic_dataset = train_df.copy()
 
-# Сохранение датасета в CSV файл
-titanic_dataset.to_csv("titanic_dataset.csv", index=False)
+# Создание нового датасета с информацией о классе, поле и возрасте
+additional_info = pd.DataFrame({
+    'Pclass': [1, 2, 3],
+    'Sex': ['male', 'female', 'male'],
+    'Age': [30, 25, 22]
+})
+
+# Объединение датасетов
+titanic_dataset_extended = pd.concat([titanic_dataset, additional_info], axis=1)
+
+# Сохранение расширенного датасета в CSV файл
+titanic_dataset_extended.to_csv("titanic_dataset_extended.csv", index=False)
 
